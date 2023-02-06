c50a7995714affbb68fbb55a21b7f799e99a471a
118littlepigs <al.alvinliu@gmail.com>
2023-02-06 11:15:11 -0800

    final

--- TMDB_scraper/TMDB_scraper/spiders/.ipynb_checkpoints/tmdb_spider-checkpoint.py
+++ TMDB_scraper/TMDB_scraper/spiders/.ipynb_checkpoints/tmdb_spider-checkpoint.py
@@ -10,27 +10,37 @@ class TmdbSpider(scrapy.Spider):
     
     def parse(self,response):
         '''
+        Assuming starts on movie page, navigates to cast page
         '''
         cast_url = response.request.url + "/cast"
         yield scrapy.Request(cast_url,callback = self.parse_full_credits)
     
     def parse_full_credits(self,response):
         '''
+        Scrapes list of actor pages from cast page and navigates to each
         '''
+        
+        #finds list of actor pages linked in cast page
         cast = response.css("section.panel.pad")[0]
         actor_pages = cast.css("div.info p a::attr(href)").getall()
+        
+        #calls parse_actor_page for each actor page in list
         for page in actor_pages:
             yield response.follow(page, callback=self.parse_actor_page)
     
     def parse_actor_page(self,response):
         '''
+        Starts on actor page, scrapes and yields movies/shows that actor has been in
         '''
+        
+        #finds actor name
         actor_name = response.css("title::text").get().split(" — ")[0]
         
+        #finds list of movies/shows that actor has been in
         credits = response.css("table.card.credits")[0]
-        credit_links = credits.css("td.role a.tooltip::attr(href)").getall()
         credit_names = credits.css("td.role a.tooltip bdi::text").getall()
         
+        #yields dict for each movie/show, corresponds to a line in .csv
         for credit in credit_names:
             yield {"actor":actor_name,"movie_or_TV_name":credit}
             
